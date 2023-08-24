6730be0a145d329c25c6c97cd6b4e00b74df969f
Albert Snow <ajsnow2012@gmail.com>
2023-08-23 23:25:38 -0500

    adding more work

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,18 +1,22 @@
 import express from 'express';
 import path from "path";
+import dotenv from "dotenv";
 import { fileURLToPath } from 'url';
+import * as sessionAuth from "./middleware/sessionAuth";
+import * as routes from "./routes";
+dotenv.config();
 const app = express();
-const port = 8080; // default port to listen
+const port = process.env.SERVER_PORT; // default port to listen
 // Configure Express to use EJS
 const FILENAME = fileURLToPath(import.meta.url);
 const DIRNAME = path.dirname(FILENAME);
 const PARENT = path.dirname(DIRNAME);
 app.set("views", path.join(PARENT, "views"));
 app.set("view engine", "ejs");
-// define a route handler for the default home page
-app.get("/", (req, res) => {
-    res.render("index");
-});
+// Configure session auth
+sessionAuth.register(app);
+// Configure routes
+routes.register(app);
 // start the Express server
 app.listen(port, () => {
     console.info(`server started at http://localhost:${port}`);
---
+++ dist/src/middleware/session_auth.d.ts
@@ -0,0 +1,1 @@
+export declare const register: (app: any) => void;
